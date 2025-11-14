dfd2d57244cf7643f132e53fc19990ae317d63a5
Bucur Alexandru <alexandru.bucur@bestcj.ro>
2025-11-14 17:13:27 +0200

    py camere

--- app/camere/page.tsx
+++ app/camere/page.tsx
@@ -49,7 +49,7 @@ export default function CamerePage() {
                </section>
 
                {/* Rooms Grid */}
-               <section className="py-12 md:py-20">
+               <section className="py-20">
                     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                          <div className="flex flex-wrap justify-center gap-8">
                               {rooms.map((room, index) => (
